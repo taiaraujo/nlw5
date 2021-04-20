76b57b0cc0a0640a2557fb2bf5e38e43aa1de534
Natália Freitas Araújo <taiaraujo20@gmail.com>
2021-04-20 10:43:57 -0300

    aula 2 - estruturação do banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
-import express, { request, response } from "express";
+import express from "express";
+
+import "./database";
+import { routes } from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({message: "Ola NLW 5"})
-})
+app.use(express.json())
 
-app.post("/", (request, response) => {
-  return response.json({message: "usuario salvo com sucesso"})
-})
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
