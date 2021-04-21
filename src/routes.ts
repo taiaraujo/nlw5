a6e68a3fb0571190c49274c2a2828da3c925be78
Natália Freitas Araújo <taiaraujo20@gmail.com>
2021-04-21 12:20:48 -0300

    aula 3 - com: SQLITEerror: no such column: Message.user_id

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
+import { MessagesController } from "./controllers/MessagesController";
 import { SettingsController } from "./controllers/SettingsController";
+import { UsersController } from "./controllers/UsersController";
 
 const routes = Router();
 
+const messagesController = new MessagesController();
 const settingsController = new SettingsController();
+const usersController = new UsersController();
 
+routes.post("/messages", messagesController.create);
 routes.post("/settings", settingsController.create);
+routes.post("/users", usersController.create);
+
 
 export { routes };
